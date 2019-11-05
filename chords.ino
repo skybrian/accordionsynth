2892d0bddc0597828989ed01c84c3d7f1cd137ea
Brian Slesinsky <brian-git@slesinsky.org>
2019-11-04 22:49:01 -0800

    introduce Bank

--- chords.ino
+++ chords.ino
@@ -9,31 +9,24 @@ using namespace midi;
 #include "sound.h"
 using namespace sound;
 
-const Chord major[] = {
+const Chord song[] = {
+  Chord(B2-1),
   Chord::major(B2-1),
-//  {F3, midi::A3, C3},
-//  {C3, E3, G3},
-//  {G3, B3, D3}
+  Chord(F3),
+  Chord::major(F3),
+  Chord(C3),
+  Chord::major(C3),
+  Chord(G3),
+  Chord(D3, G3, B3),
 };
 
 AudioControlSGTL5000 shield;
-Reed reeds[] = {
-  Reed(C3),
-  Reed(E3),
-  Reed(F3),
-  Reed(G3),
-};
-AudioMixer4 mixer;
+Bank bank(B2-1);
 AudioOutputI2S out;
 
 AudioConnection patches[] = {
-  AudioConnection(reeds[0].env, 0, mixer, 0),  
-  AudioConnection(reeds[1].env, 0, mixer, 1),  
-  AudioConnection(reeds[2].env, 0, mixer, 2),  
-  AudioConnection(reeds[3].env, 0, mixer, 3),
-
-  AudioConnection(mixer, 0, out, 0),
-  AudioConnection(mixer, 0, out, 1),  
+  AudioConnection(bank.mixer, 0, out, 0),
+  AudioConnection(bank.mixer, 0, out, 1),  
 };
 
 #define LENGTH(x) (sizeof(x)/sizeof(x[0]))
@@ -41,45 +34,16 @@ AudioConnection patches[] = {
 void setup() {
   Serial.begin(9600);
   AudioMemory(20);
-
-  float reedGain = 0.8/LENGTH(reeds);
-  for(unsigned i = 0; i < LENGTH(reeds); i++) {    
-    mixer.gain(i, reedGain);
-  }
-  
   shield.enable();
   shield.volume(0.25);
 }
 
 
 void loop() {
-  for (unsigned i = 0; i < LENGTH(reeds); i++) {
-    reeds[i].noteOn();
+  for (unsigned i = 0; i < LENGTH(song); i++) {
+    bank.notesOn(song[i]);
     delay(200);
-    reeds[i].noteOff();
+    bank.notesOff();
     delay(200);    
   }
-  
-//  for (unsigned int i = 0; i < LENGTH(major); i++) {
-//    wave1.frequency((major[i][0]-12).frequency());    
-//    wave2.frequency(major[i][1].frequency());
-//    wave3.frequency(major[i][2].frequency());
-//
-//    wave2.amplitude(0);
-//    wave3.amplitude(0);
-//
-//    env.noteOn();
-//    delay(200);
-//    env.noteOff();
-//    delay(200);    
-//
-//    wave1.frequency(major[i][0].frequency());    
-//    wave2.amplitude(1.0);
-//    wave3.amplitude(1.0);
-//
-//    env.noteOn();
-//    delay(200);
-//    env.noteOff();
-//    delay(200);        
-//  }
 }
--- sound.h
+++ sound.h
@@ -1,16 +1,20 @@
 #include "midi.h"
 #include <Audio.h>
+#include <AudioStream.h>
 
 namespace sound {
 
 class Reed {
 public:
-  Reed(midi::Note n) {
+  Reed(midi::Note n) : note(n) {
     wave.amplitude(1.0);
     wave.frequency(n.frequency());
-    wave.begin(WAVEFORM_SAWTOOTH);
     env.sustain(1.0);
     env.release(10);
+  }
+
+  void begin() {
+    wave.begin(WAVEFORM_SAWTOOTH);    
   }
 
   void noteOn() {
@@ -22,9 +26,101 @@ public:
   }
 
   AudioEffectEnvelope env;
+  midi::Note note;
 private:
   AudioSynthWaveform wave;
   AudioConnection patch = AudioConnection(wave, 0, env, 0);
+};
+
+#if !defined(__ARM_ARCH_7EM__)
+#error "BigMixer not implemented for this platform"
+#endif
+
+#define MIXER_SIZE 12
+
+class BigMixer : public AudioStream
+{
+public:
+  BigMixer() : AudioStream(MIXER_SIZE, inputQueueArray) {
+    for (int i=0; i<MIXER_SIZE; i++) {
+      multiplier[i] = 65536;
+    }
+  }
+
+  virtual void update(void);
+
+  void gain(unsigned int channel, float gain) {
+    if (channel >= MIXER_SIZE) return;
+    if (gain > 32767.0f) gain = 32767.0f;
+    else if (gain < -32767.0f) gain = -32767.0f;
+    multiplier[channel] = gain * 65536.0f; // TODO: proper roundoff?
+  }
+  
+private:
+  int32_t multiplier[MIXER_SIZE];
+  audio_block_t *inputQueueArray[MIXER_SIZE];
+};
+
+#define BANK_SIZE MIXER_SIZE
+
+struct ReedBank {
+  Reed at[BANK_SIZE];
+};
+
+class Bank {
+private:
+  static ReedBank makeReeds(midi::Note n) {
+    ReedBank reeds = {{
+      Reed(n), Reed(n+1), Reed(n+2), Reed(n+3), Reed(n+4), Reed(n+5),
+      Reed(n+6), Reed(n+7), Reed(n+8), Reed(n+9), Reed(n+10), Reed(n+11)
+    }};
+    return reeds;
+  }
+
+public:
+  Bank(midi::Note n) : reeds(makeReeds(n)) {
+    for (int i = 0; i < BANK_SIZE; i++) {
+      reeds.at[i].begin();
+    }
+  }
+
+  void notesOn(midi::Chord chord) {
+    int noteCount = 0;
+    for (int i = 0; i<BANK_SIZE; i++) {
+      if (chord.has(reeds.at[i].note)) {
+        reeds.at[i].noteOn();
+        noteCount++;
+      } else {
+        reeds.at[i].noteOff();
+      }
+    }
+    if (noteCount > 0) {
+      float gain = 0.8/noteCount;
+      for (int i = 0; i<BANK_SIZE; i++) {
+        mixer.gain(i, gain);
+      }
+    }
+  }
+
+  void notesOff() {
+    notesOn(midi::Chord());
+  }
+  
+  BigMixer mixer;
+private:
+  ReedBank reeds;
+  AudioConnection patch1 = AudioConnection(reeds.at[0].env, 0, mixer, 0);
+  AudioConnection patch2 = AudioConnection(reeds.at[1].env, 0, mixer, 1);
+  AudioConnection patch3 = AudioConnection(reeds.at[2].env, 0, mixer, 2);
+  AudioConnection patch4 = AudioConnection(reeds.at[3].env, 0, mixer, 3);
+  AudioConnection patch5 = AudioConnection(reeds.at[4].env, 0, mixer, 4);
+  AudioConnection patch6 = AudioConnection(reeds.at[5].env, 0, mixer, 5);
+  AudioConnection patch7 = AudioConnection(reeds.at[6].env, 0, mixer, 6);
+  AudioConnection patch8 = AudioConnection(reeds.at[7].env, 0, mixer, 7);
+  AudioConnection patch9 = AudioConnection(reeds.at[8].env, 0, mixer, 8);
+  AudioConnection patch10 = AudioConnection(reeds.at[9].env, 0, mixer, 9);
+  AudioConnection patch11 = AudioConnection(reeds.at[10].env, 0, mixer, 10);
+  AudioConnection patch12 = AudioConnection(reeds.at[11].env, 0, mixer, 11);
 };
 
 } // namespace
